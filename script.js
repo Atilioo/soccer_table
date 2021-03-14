b21e6658feaf1e74e490f0ffd0d303ac55a3bdac
FUNDAC <fundac@jatai.go.gov.br>
2021-03-14 11:05:18 -0300

    estruturação da página

--- script.js
+++
@@ -1,9 +0,0 @@
-var sanduiche = document.querySelector(".sanduiche");
-var banner = document.querySelectorAll(".banner")
-sanduiche.addEventListener("click", function(){
-    document.querySelector(".sidebar").classList.toggle("mostrar-menu");
-    banner[0].classList.toggle("add-blur")
-    banner[1].classList.toggle("add-blur")
-    document.querySelector(".img-new").classList.toggle("add-blur")
-    
-});
